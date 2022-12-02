058dd58e6e7e29feb9881d1b9d79266e67cf3708
Helena Spiewak <helena.l.spiewak@gmail.com>
2022-12-02 21:16:23 +0000

    Day 2 solution

--- day_2.py
+++ day_2.py
@@ -2,23 +2,71 @@
 ### Author: Helena L. Spiewak
 ### Date created: 2022-12-02
 
-# Rock defeats Scissors, Scissors defeats Paper, 
-# and Paper defeats Rock. If both players choose the same shape, 
+# Rock defeats Scissors, Scissors defeats Paper,
+# and Paper defeats Rock. If both players choose the same shape,
 # the round instead ends in a draw.
 
-# dict of moves
-opponent = {"A": "Rock", "B" : "Paper", "C": "Scissors"}
-response = {"X": "Rock", "Y" : "Paper", "Z": "Scissors"}
-scores = {"Rock": 1, "Paper": 2, "Scissors": 3}
+import os
 
-def parse_strategy_guide(strategy_guide):
+# dict of keys, scores and which defeats which
+opponent = {"A": "Rock", "B": "Paper", "C": "Scissors"}
+scores = {"Rock": 1, "Paper": 2, "Scissors": 3}
+defeats = {"Rock": "Scissors", "Scissors": "Paper", "Paper": "Rock"}
+
+
+def per_round_score(strategy_guide):
+    """ Function that takes a tab delimited text file which is a strategy guide:
+    :param strategy_guide: Text file.
+    :return: Dict of score for each round"""
+    all_round_scores = {}
     with open(strategy_guide, "r") as file:
+        # round counter
+        round_n = 0
+        # read each line
         for round in file:
-            choices = round.replace("\n", "").split("\t")
-            print(choices)
-            if choices[0] == choices[1]:
-                # draw
-                
+            round_n += 1
+            # reset score for each round
+            round_score = 0
+            shapes = round.replace("\n", "").split(" ")
+
+            # get opponent shape
+            opp_choice = opponent[shapes[0]]
+            # get which outcome should happen
+            outcome = shapes[1]
+
+            # draw
+            if outcome == "Y":
+                # add outcome score
+                round_score += 3
+                # what shape is needed for this to happen
+                your_choice = opp_choice
+            # lose
+            elif outcome == "X":
+                round_score += 0
+                your_choice = defeats[opp_choice]
+            # win
+            else:
+                round_score += 6
+                your_choice = [
+                    key for key, val in defeats.items() if val == opp_choice
+                ][0]
+
+            # add choice score
+            round_score += scores[your_choice]
+            # add to dictionary
+            all_round_scores[round_n] = round_score
+
+        print("Score for rounds = {}".format(all_round_scores))
+
+    return all_round_scores
+
+
+def total_scores(all_round_scores):
+    """ Function sum the individual round scores and return total.
+    :param all_round_scores: Dict of integers:
+    :return: Total of input Dict """
+    return sum(all_round_scores.values())
+
 
 # unit tests
 def create_test_file(stategies):
@@ -37,11 +85,23 @@ def create_test_file(stategies):
     return test_file_path
 
 
-def test_expected_output_last():
+def test_expected_output_per_round_score():
     # create test text file
-    strategy = create_test_file(["A\tY", "B\tX", "C\tZ"])
+    strategy = create_test_file(["A Y", "B X", "C Z"])
 
     # test function
-    assert parse_strategy_guide(strategy_guide=strategy) == {1: 2200, 2: 110, 3: 10000}
+    assert per_round_score(strategy_guide=strategy) == {1: 4, 2: 1, 3: 7}
     # remove file
     os.remove(strategy)
+
+
+def test_total_scores():
+    assert total_scores(all_round_scores={1: 4, 2: 1, 3: 7}) == 12
+
+
+# run against input file
+# parse input file
+scores_per_round = per_round_score(strategy_guide="./input_day2.txt")
+# puzzle 1
+total = total_scores(scores_per_round)
+print("Total score = {}".format(total))
